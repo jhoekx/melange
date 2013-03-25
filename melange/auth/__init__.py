b732f663ae1b5014a812336c941d754f7e335de3
Jeroen Hoekx <jeroen@hoekx.be>
2013-03-25 18:19:19 +0100

    Add license.

--- melange/auth/__init__.py
+++ melange/auth/__init__.py
@@ -1,3 +1,20 @@
+# (c) 2013, Jeroen Hoekx <jeroen.hoekx@dsquare.be>
+#
+# This file is part of Melange.
+#
+# Melange is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# Melange is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with Melange.  If not, see <http://www.gnu.org/licenses/>.
+
 from functools import wraps
 
 from flask import Blueprint, abort, g, redirect, request, session, url_for, make_response, render_template
