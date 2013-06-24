29f83b55f139a74d4910a4e9e0c8b3dcf5644095
Jeroen Hoekx <jeroen@hoekx.be>
2013-06-24 14:15:38 +0200

    Add HTTP API for Ansible.

--- runserver.py
+++ runserver.py
@@ -6,6 +6,7 @@ from optparse import OptionParser
 parser = OptionParser()
 parser.add_option('-i', '--initdb', default=None, action='store_true', dest='initdb')
 parser.add_option('-d', '--dropdb', default=None, action='store_true', dest='dropdb')
+parser.add_option('-a', '--ansibleapi', default=None, action='store_true', dest='ansibleapi')
 options, args = parser.parse_args()
 
 if options.initdb:
@@ -20,6 +21,9 @@ elif options.dropdb:
     from melange.database import drop_db
     print 'Dropping database'
     drop_db()
+elif options.ansibleapi:
+    from ansible_api import app
+    app.run(port=5001, debug=True)
 else:
     from melange import app
     app.run()
